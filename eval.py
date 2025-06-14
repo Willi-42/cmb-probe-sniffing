ce21277ae8379d88288c49de6578a947eef0259e
Fabian Fromwald <fabian@fromwald.at>
2025-06-14 16:26:08 +0200

    Check singal strength

--- eval.py
+++ eval.py
@@ -4,8 +4,7 @@ import binascii
 from scapy.all import rdpcap
 
 
-def check_mac(mac):
-
+def is_mac_gloabl(mac):
     macbytes = binascii.unhexlify(mac.replace(':', ''))
 
     first_byte = macbytes[0]
@@ -13,14 +12,23 @@ def check_mac(mac):
     bit = (first_byte >> 1) & 1
 
     if bit == 1:
-        print("local")
-    elif bit == 0:
-        print("global")
+        return False
+
+    return True
+
+
+def is_signal_good(rssi):
+    if rssi >= -80:
+        return True
+    return False
 
 
 def main():
     """Starts the script"""
-    packets = rdpcap("./outfile.pcap")
+    packets = rdpcap("./data.pcap")
+
+    local_cnt = 0
+    good_rssi_cnt = 0
 
     for p in packets:
         timestamp = p.time
@@ -29,10 +37,20 @@ def main():
         rssi = radio_tap.dBm_AntSignal
 
         src = radio_tap.addr2
+        is_global = is_mac_gloabl(src)
+        is_rssi_good = is_signal_good(rssi)
 
-        print(f"{src}, {timestamp}, {rssi}")
+        # print(f"{src}, {timestamp}, {rssi} {'good' if is_rssi_good else 'bad'}, {'global' if is_global else 'local'}")
 
-        check_mac(src)
+        if is_rssi_good:
+            good_rssi_cnt += 1
+
+        if is_global:
+            local_cnt += 1
+
+    global_cnt = len(packets) - local_cnt
+    print(f"Local: {local_cnt}\nGlobal: {global_cnt}")
+    print(f"Good Signal: {good_rssi_cnt}")
 
 
 if __name__ == "__main__":
