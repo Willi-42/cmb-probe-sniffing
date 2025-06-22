cfdda86ccd4b749641676dbb16aa7e7fd6e15382
Fabian Fromwald <fabian@fromwald.at>
2025-06-22 16:55:43 +0200

    Capture on two interfaces at the same time

--- run_capture.sh
+++ run_capture.sh
@@ -1,15 +1,33 @@
-INTERFACE_NAME="wlx24050fe5804b"
+INTERFACE_NAME_1="wlx24050fe57a7d"
+INTERFACE_NAME_2="wlx24050fe588e9"
 DURATION_SEC=100
 
 # setup interface
-sudo ip link set $INTERFACE_NAME down
-sudo iwconfig $INTERFACE_NAME mode monitor
-sudo ip link set $INTERFACE_NAME up
+sudo ip link set $INTERFACE_NAME_1 down
+sudo iwconfig $INTERFACE_NAME_1 mode monitor
+sudo ip link set $INTERFACE_NAME_1 up
+
+sudo ip link set $INTERFACE_NAME_2 down
+sudo iwconfig $INTERFACE_NAME_2 mode monitor
+sudo ip link set $INTERFACE_NAME_2 up
+
+
+sudo iw $INTERFACE_NAME_1 set channel 1
+sudo iw $INTERFACE_NAME_2 set channel 11
+
+
+# sudo iw $INTERFACE_NAME_1 info
+# sudo iw $INTERFACE_NAME_2 info
+
 
 # sniff
-tshark -i $INTERFACE_NAME -a duration:$DURATION_SEC -f "wlan type mgt subtype probe-req" -w data.pcap
+tshark -f "wlan type mgt subtype probe-req" -i $INTERFACE_NAME_2 -i $INTERFACE_NAME_1 -a duration:$DURATION_SEC  -w data.pcap 
 
 # reset interface
-sudo ip link set $INTERFACE_NAME down
-sudo iwconfig $INTERFACE_NAME mode managed
-sudo ip link set $INTERFACE_NAME up
+sudo ip link set $INTERFACE_NAME_1 down
+sudo iwconfig $INTERFACE_NAME_1 mode managed
+sudo ip link set $INTERFACE_NAME_1 up
+
+sudo ip link set $INTERFACE_NAME_2 down
+sudo iwconfig $INTERFACE_NAME_2 mode managed
+sudo ip link set $INTERFACE_NAME_2 up
